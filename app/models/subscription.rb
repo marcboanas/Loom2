981bb4df142e993e1443984bc756febfa958e9d7
Marc <marc@Marcs-iMac.local>
2013-10-01 10:18:52 +0100

    stripe

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -69,7 +69,8 @@ class Subscription < ActiveRecord::Base
     end
     def payment_success(event)
         self.failed_payment = false
-        self.payment_history = [{"amount" => event.data.object.lines.data[0].amount}]
+        self.payment_history = [] unless self.payment_history
+        self.payment_history = self.payment_history.push [{"amount" => event.data.object.lines.data[0].amount}]
         self.email = 'well@done.com'
         self.save
     end
