b835bba6b3c44c39e60b389052089acb8a950d23
Marc <marc@Marcs-iMac.local>
2013-10-01 08:32:58 +0100

    stripe

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -68,7 +68,7 @@ class Subscription < ActiveRecord::Base
     end
     def payment_success(customer)
         self.failed_payment = false
-        self.email = customer.id
+        self.email = customer
         self.save
     end
 end
