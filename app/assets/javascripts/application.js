996dc610e553d3d522cdc6c64b55b20ace2dfe6a
Marc <marc@Marcs-iMac.local>
2013-10-14 18:25:23 +0100

    update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -237,6 +237,17 @@ $(function() {
                      
         });
   
+  $(document).mouseup(function (e)
+                      {
+                      var container = $('.drop-menu');
+                      
+                      if (!container.is(e.target) // if the target of the click isn't the container...
+                          && container.has(e.target).length === 0) // ... nor a descendant of the container
+                      {
+                      container.slideUp();
+                      }
+                      });
+  
 });
 
 
