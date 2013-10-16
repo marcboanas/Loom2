74540e0a9d94cec7fac0bdf19119085440380889
Marc <marc@Marcs-iMac.local>
2013-10-16 20:59:35 +0100

    update income

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -2,7 +2,7 @@ class Expense < ActiveRecord::Base
     attr_accessible :asset_life, :dated, :description, :expense_type_id, :user_id, :value, :private_use, :sale_value, :sold
     belongs_to :user
     belongs_to :expense_type
-    has_many :incomes
+    has_many :incomes, :dependent => :destroy
     
     def short_description
         short_description = (!self.description.empty?) ? self.description : "no description"
