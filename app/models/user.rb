59e7e7f6384cbd20554d1598e150ac6170e7da33
Marc <marc@Marcs-iMac.local>
2013-10-22 21:12:12 +0100

    email 2

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,8 +60,10 @@ class User < ActiveRecord::Base
     end
     
     def send_password_reset
+        generate_token(:password_reset_token)
         self.password_reset_sent_at = Time.zone.now
         save!
+        UserMailer.password_reset(self).deliver
     end
     
     private
