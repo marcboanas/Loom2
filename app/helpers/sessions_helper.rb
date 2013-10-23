91223652c00253da90ad1b7648e6263bb7326bd0
Marc <marc@Marcs-iMac.local>
2013-10-23 14:54:18 +0100

    password reset

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,12 +3,13 @@ module SessionsHelper
     def sign_in(user)
         if params[:session][:remember_me] == "1"
             cookies.permanent[:remember_token] = user.remember_token
-            user.remember_me! = true
+            user.remember_me = true
             else
             cookies[:remember_token] = user.remember_token
-            user.remember_me! = false
+            user.remember_me = false
         end
         self.current_user = user
+        self.save!
     end
     
     
